06a9ed0779af62c57ae42fcff1eb1536c89a5ab2
jonathan <jonathanxavier556@gmail.com>
2025-07-08 22:37:34 -0300

    criando html da pagina

---
+++ script.js
@@ -0,0 +1,11 @@
+// Retorne o url da página atual utilizando o objeto window
+const url = window.location.href
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+
+const elementoAtivo = document.querySelector('.ativo')
+
+// Retorne a linguagem do navegador
+const language = window.navigator.language
+// Retorne a largura da janela 
+window.innerWidth
